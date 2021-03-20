d12dcffb2395d3f61e349b17a7b6d0ed93e92d75
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2021-03-20 14:36:26 +0100

    first commit

---
+++ src/Modal.js
@@ -0,0 +1,17 @@
+import React, {Component } from 'react';
+
+
+class Modal extends Component {
+
+    render() {
+        return (
+            <div className="modal">
+                <div>
+                    Hello world
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Modal;
